117e569e0255ef913055084ef65a9aef9d8a400e
nistorrazvaniulian <nistorrazvaniulian@gmail.com>
2025-06-06 16:56:41 +0300

    curățare: eliminare credentiale Google expuse și actualizare .env cu secrete noi

--- eventchain-backend/src/config/keys.js
+++ eventchain-backend/src/config/keys.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   mongoURI: process.env.MONGODB_URI,
   googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   jwtSecret: process.env.JWT_SECRET
 };
